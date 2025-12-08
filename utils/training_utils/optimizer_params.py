89eb718c00dacbe5e150e7615a3024a358959415
Ananthu Aniraj <ananthu.aniraj@inria.fr>
2025-12-08 17:31:15 +0100

    add efficient pooling baseline

--- utils/training_utils/optimizer_params.py
+++ utils/training_utils/optimizer_params.py
@@ -93,15 +93,16 @@ def layer_group_matcher_baseline(args, model, dataset_train):
     """
     weight_decay = calculate_weight_decay(args, dataset_train)
     scratch_layers = ["head.", "fc.", "attn_pool.", "sim_pool", "fc_norm", "efficient_pool"]
+    scratch_layers_no_wd = ["efficient_pool.cls_token"]
     scratch_parameters = []
-    no_weight_decay_params_scratch = ["efficient_pool.cls_token"]
+    no_weight_decay_params_scratch = []
     finetune_parameters = []
     no_weight_decay_params_bb = []
     for name, p in model.named_parameters():
 
         if any(x in name for x in scratch_layers):
             print("scratch layer_name: " + name)
-            if p.ndim == 1 or name in no_weight_decay_params_scratch:
+            if p.ndim == 1 or any(x in name for x in scratch_layers_no_wd):
                 no_weight_decay_params_scratch.append(p)
             else:
                 scratch_parameters.append(p)
